cea051e57d05de7e873589d0a193016cdedc7197
standyoon <81007682+Byunseoyoon@users.noreply.github.com>
2024-08-28 15:50:28 +0900

    이미지 추가

--- app/src/main/java/com/example/freshtime/MainActivity.java
+++ app/src/main/java/com/example/freshtime/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.freshtime;
 
+import static java.security.AccessController.getContext;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.example.freshtime.domain.Food;
+import com.example.freshtime.domain.Refrigerator;
+import com.example.freshtime.repository.RefrigeratorRepository;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +23,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.main_bnv);
+
+        //더미데이터 넣기
+        // 냉장고 이름 저장
+        Refrigerator newFridge = new Refrigerator("main");
+        RefrigeratorRepository.save(newFridge);
+        // 선택한 냉장고 이름으로 Refrigerator 객체 가져오기
+        Refrigerator selectedFridge = RefrigeratorRepository.getRefrigeratorByName("main");
+
+        // Food 객체 생성 및 냉장고에 추가
+        Food newFood = new Food("banana", "2024/09/05", 5);
+        selectedFridge.addFood(newFood);
+        Food newFood2 = new Food("배", "2024/08/03", 2);
+        selectedFridge.addFood(newFood2);
+
 
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             Fragment selectedFragment = null;
